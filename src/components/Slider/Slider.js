f40bef9819f22756e7d7b1cf7a0da59c5e21ebea
Azat Siu <umraliyev.azvt@gmail.com>
2024-02-04 18:14:45 +0400

    Finished collections slider on homepage

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -4,28 +4,55 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import "./Slider.css";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import "swiper/css/effect-coverflow";
+
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  EffectCoverflow,
+} from "swiper/modules";
 
 export const Slider = ({ slides }) => {
   return (
-    <Swiper
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
-      slidesPerView={2}
-      navigation
-      pagination={{ clickable: true }}
-      onSlideChange={() => console.log("slide change")}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      {slides.map((slide) => (
-        <SwiperSlide key={slide.id}>
-          <div className="slide">
-            <img src={slide.image} alt="cloth photo" />
-            <h3>{slide.title}</h3>
-            <p>{slide.description}</p>
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className="Swiper-container">
+      <Swiper
+        effect={"coverflow"}
+        className="mySwiper"
+        modules={[Navigation, Pagination, A11y, Pagination, EffectCoverflow]}
+        spaceBetween={50}
+        glabCursor={true}
+        slidesPerView={3}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        navigation
+        pagination={{ clickable: true }}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        {slides.map((slide) => (
+          <SwiperSlide key={slide.id}>
+            <div className="slide">
+              <img
+                src={slide.image}
+                alt="cloth photo"
+                className="slide-image"
+              />
+              <div className="slider-info">
+                <h3>{slide.title.toUpperCase()}</h3>
+                <p>{slide.description}</p>
+                <a href="/shop">Discover more!</a>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
